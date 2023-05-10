b2fbfbf09d564fdd8cec38784bc699080ea2913d
unknown <dnal03@msn.com>
2023-05-10 13:51:08 -0400

    decimal/double correction

--- src/DivideMethod.java
+++ src/DivideMethod.java
@@ -2,9 +2,10 @@ public class DivideMethod {
     public static void main (String[] args) {
         divide(1,3);
     }
-    public static double divide(double num1, double num2) {
-        double d = num1 / num2;
-        System.out.println(num1 +" / " + num2 + " = " + (num1/num2));
+    public static double divide(int num1, int num2) {
+        double d = (double)num1 / num2;
+
+        System.out.println(num1 +" / " + num2 + " = " + d);
 
         return d;
     }
